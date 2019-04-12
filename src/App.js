587cebfe3a893546d9dae8aa128da5b9d52816a7
Kim McGrath <klmcgrath2@gmail.com>
2019-04-12 16:47:01 +0100

    modal again

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ReactGA from "react-ga";
+import Modal from "../src/components/Modal";
 
 function initializeReactGA() {
   ReactGA.initialize("UA-138224838-1");
@@ -18,6 +19,26 @@ function initializeReactGA() {
 // };
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isShowing: false
+    };
+  }
+
+  openModalHandler = () => {
+    this.setState({
+      isShowing: true
+    });
+  };
+
+  closeModalHandler = () => {
+    this.setState({
+      isShowing: false
+    });
+  };
+
   onButtonClick = () => {
     console.log("clicked");
     ReactGA.event({
@@ -28,23 +49,44 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <button onClick={this.onButtonClick}>Click Me </button>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <button onClick={this.onButtonClick}>Click Me </button>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+
+        <div>
+          {this.state.isShowing ? (
+            <div onClick={this.closeModalHandler} className="back-drop" />
+          ) : null}
+
+          <button className="open-modal-btn" onClick={this.openModalHandler}>
+            Open Modal
+          </button>
+
+          <Modal
+            className="modal"
+            show={this.state.isShowing}
+            close={this.closeModalHandler}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+            Maybe aircrafts fly very high because they don't want to be seen in
+            plane sight?
+          </Modal>
+        </div>
+      </>
     );
   }
 }
